5bd907f5c7538f205bbcdd80813e388f020880c7
Crisna Bezerra <crisnabto@gmail.com>
2022-10-11 18:20:44 -0300

    Teste git req 2

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,10 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 3306,
-  user: 'root',
-  password: 'root',
-  database: 'StoreManager',
+  user: process.env.MYSQL_USER || 3306,
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
